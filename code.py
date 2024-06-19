31390ee34c807fc1818e92ff5f01444e19503940
aksinghania <akshtsinghnia@gmail.com>
2024-06-20 00:47:52 +0530

    added pathfinding

--- code.py
+++ code.py
@@ -93,15 +93,15 @@ def h(p1,p2):
     x2,y2 = p2
     return abs(x1-x2) + abs(y1-y2)
 
-def algorithm(draw,grid,start,end):
+def algorithm(draw, grid, start, end):
     count = 0
     open_set = PriorityQueue()
-    open_set.put((0,count,start))
+    open_set.put((0, count, start))
     came_from = {}
     g_score = {spot: float("inf") for row in grid for spot in row}
     g_score[start] = 0
     f_score = {spot: float("inf") for row in grid for spot in row}
-    f_score[start] = h(start.get_pos(),end.get_pos())
+    f_score[start] = h(start.get_pos(), end.get_pos())
 
     open_set_hash = {start}
 
@@ -109,11 +109,13 @@ def algorithm(draw,grid,start,end):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
-        
+
         current = open_set.get()[2]
         open_set_hash.remove(current)
 
         if current == end:
+            reconstruct_path(came_from, end, draw)
+            end.make_end()
             return True
 
         for neighbor in current.neighbors:
@@ -122,18 +124,19 @@ def algorithm(draw,grid,start,end):
             if temp_g_score < g_score[neighbor]:
                 came_from[neighbor] = current
                 g_score[neighbor] = temp_g_score
-                f_score[neighbor] = temp_g_score + h(neighbors.get_pos(),end.get_pos())
+                f_score[neighbor] = temp_g_score + h(neighbor.get_pos(), end.get_pos())
                 if neighbor not in open_set_hash:
                     count += 1
-                    open_set.push((f_score[neighbor], count, neighbor))
+                    open_set.put((f_score[neighbor], count, neighbor))
                     open_set_hash.add(neighbor)
                     neighbor.make_open()
+
         draw()
 
         if current != start:
             current.make_closed()
-    return False
 
+    return False
 
 
 def make_grid(rows,width):
